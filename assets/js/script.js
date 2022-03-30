7a855df7f0003a31d582bb24517d93e039b92f6c
sherryrich <ricsherry@gmail.com>
2022-03-30 07:55:42 +0000

    added greeting script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-alert("HELLO")
+/* This script displays a greeeting to the user bacsed upon the current Time
+it is an example from Javascript and jquest book */
+
+let today = new Date(); // create a new date object
+let hourNow = today.getHours(); // Find the current hour
+let greeting;
+
+//Display the appropriate greeting bacsed on the current time
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
