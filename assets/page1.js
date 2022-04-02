3c42bab2a35250d9f244c10fb08aea69bcf2506b
sherryrich <ricsherry@gmail.com>
2022-04-02 09:27:05 +0000

    update

--- assets/page1.js
+++ assets/page1.js
@@ -1,9 +1,27 @@
+//seclcting HTML elements with Javascript
 document.querySelector("h1").innerHTML = "Richard";
 document.firstElementChild.lastElementChild.firstElementChild;
 var heading = document.firstElementChild.lastElementChild.firstElementChild;
 
-heading.innerHTML = "Good Nye";
+heading.innerHTML = "Good Bye";
 
 document.querySelector("input").click();
 
 heading.style.color = "blue";
+
+document.getElementsByTagName("li")[2].style.color = "red";
+
+document.getElementsByClassName("btn")[0].style.color = "orange";
+
+document.getElementById("title").innerHTML = "Hello Again from JS";
+
+document.querySelector("#title") // just like ID selector in css
+
+document.querySelector(".btn") // just like class selector in css
+
+document.querySelector("li a") // combine selectors, specific
+
+document.querySelector("li.item") // combining selector, no spaces because located on the same level element 
+
+document.querySelector("#list a") // anchor tag that is inside somehting with id of list, grandchild
+
